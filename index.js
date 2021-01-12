877984d92dcd13a0cc30dc6c42f10d285468143e
Charlie Knight <williamchknight@gmail.com>
2021-01-12 12:57:38 -0800

    commit tree 0cff66323d87f1a86c68c7d27f36b474b87be5ec

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Your code goes here
 
-document.addEventListener("DOMContentLoaded", function() {
-    loaded()
+document.addEventListener("DOMContentLoaded", () => {
+    text = document.getElementById('text')
+    text.innerHTML = 'This is really cool!'
   });
 
-  function loaded() {
-      text = document.getElementById('text')
-      text.innerHTML = 'This is really cool!'
-  }
+//   function loaded() {
+//       text = document.getElementById('text')
+//       text.innerHTML = 'This is really cool!'
+//   }
