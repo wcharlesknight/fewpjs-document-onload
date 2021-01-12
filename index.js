1814c795f1987fbfddd0b32ac943dded47586273
Charlie Knight <williamchknight@gmail.com>
2021-01-12 12:54:42 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    loaded()
+  });
+
+  function loaded() {
+      text = document.getElementById('text')
+      text.innerHTML = 'This is really cool!'
+  }
